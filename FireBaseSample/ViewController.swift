43b0b824158689f77382ebe37bbeef2910febed5
Kousuke Kikuchi <kousukekikuchi1984@gmail.com>
2017-02-10 21:51:29 +0900

    Initial Commit

---
+++ FireBaseSample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FireBaseSample
+//
+//  Created by 菊地弘祐 on 2017/02/10.
+//  Copyright © 2017 kou2kkkt. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
